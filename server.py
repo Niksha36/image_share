81d0e101c13e4cacaedadecd1c41c384a8fed953
Nikit Shuro <i.niksha36@gmail.com>
2024-09-27 17:49:35 +1000

    bug fix: server freeze

--- server.py
+++ server.py
@@ -1,16 +1,15 @@
 import socket
 import threading
-
-
+import time
 
 class Server:
     def __init__(self, chunk_size, file_name, port):
         self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server.bind((socket.gethostbyname(socket.gethostname()), port))
-        
+
         self.server.listen(4)
         self.is_close = False
-        
+
         self.chunk_size = chunk_size
         self.file_name = file_name
 
@@ -19,17 +18,23 @@ class Server:
             try:
                 if not self.file_name: continue
 
-                file = open(self.file_name, mode="rb")
+                # Move file reading and sending to a separate thread
+                threading.Thread(target=self.send_file, args=(client_socket,), daemon=True).start()
+            except:
+                pass
+
+    def send_file(self, client_socket):
+        try:
+            with open(self.file_name, mode="rb") as file:
                 data = file.read(self.chunk_size)
 
                 while data:
                     client_socket.send(data)
                     data = file.read(self.chunk_size)
-            
-                file.close()
-                self.file_name = None
-            except: 
-                pass
+            self.file_name = None
+        except Exception as e:
+            print(e)
+            pass
 
     def accept_clients(self):
         while not self.is_close:
